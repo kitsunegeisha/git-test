044eea6b7724772e8d0385339ff64ba7d5b9eb0a
kitsunegeisha <fuadkassim93@gmail.com>
2023-02-01 11:00:51 +0800

    Original

---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace vs28torbat\models;
+
+use Yii;
+use common\models\MariaDB\ManualMariaDB;
+
+class MotorblockDB
+{
+    public function ChangeWorkorder($line, $data)
+    {
+        $rpy = "1;OK";
+        $cnt2 = 1;
+        return $rpy;
+    }
+}
