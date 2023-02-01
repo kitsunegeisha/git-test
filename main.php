2550f601744a53e73c04d734b99c177372f25645
kitsunegeisha <fuadkassim93@gmail.com>
2023-02-01 11:05:08 +0800

    Changes in return value

--- main.php
+++ main.php
@@ -10,7 +10,8 @@ class MotorblockDB
     public function ChangeWorkorder($line, $data)
     {
         $rpy = "1;OK";
-        $cnt2 = 1;
+        $cnt2 = "2;OK";
+        $rpy = $cnt2;
         return $rpy;
     }
 }
